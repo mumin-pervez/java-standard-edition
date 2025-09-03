c4873b8ccac87f4a8adec25902bbd1e751d73b95
mumin-pervez <pervez.ist@gmail.com>
2025-09-04 01:20:36 +0600

    Primitive data types

---
+++ src/variables/PrimitiveVariables.java
@@ -0,0 +1,11 @@
+package variables;
+
+public class PrimitiveVariables {
+
+  public static void main(String[] args) {
+    System.out.println("Primitive Data Types");
+    System.out.println("There are 8 primitive data types");
+    System.out.println("byte, short, int, long, float, double, char, boolean");
+  }
+
+}
