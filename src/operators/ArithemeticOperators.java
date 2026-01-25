30b9a699551243e4123b6be4653c1c7323388c90
mumin-pervez <pervez.ist@gmail.com>
2026-01-25 23:30:11 +0600

    Added arithmetic operators

---
+++ src/operators/ArithemeticOperators.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class ArithemeticOperators {
+
+  public static void main(String[] args) {
+    int a = 5, b = 3;
+    System.out.println(a + "+" + b + "=" + (a + b));
+    System.out.println(a + "-" + b + "=" + (a - b));
+    System.out.println(a + "*" + b + "=" + (a * b));
+    System.out.println(a + "/" + b + "=" + (a / b));
+    System.out.println(a + "%" + b + "=" + (a % b));
+
+    System.out.println(6&12);
+  }
+
+}
