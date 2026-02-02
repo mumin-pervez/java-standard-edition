f1635ec1a49b015a911f10945f9e69e583286ba8
mumin-pervez <pervez.ist@gmail.com>
2026-02-02 11:19:25 +0600

    Initialization is must for local variables

---
+++ src/variables/LocalVarDefaultValues.java
@@ -0,0 +1,10 @@
+package variables;
+
+public class LocalVarDefaultValues {
+
+  public static void main(String[] args) {
+    int a = 10; // Local variables must be initialized for accessing
+    System.out.println(a);
+  }
+
+}
