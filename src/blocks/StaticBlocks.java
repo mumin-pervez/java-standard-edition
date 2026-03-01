037472c905f73078beab310ac08af14aa6993579
mumin-pervez <pervez.ist@gmail.com>
2026-03-02 00:44:12 +0600

    added static block example

---
+++ src/blocks/StaticBlocks.java
@@ -0,0 +1,13 @@
+package blocks;
+
+public class StaticBlocks {
+
+  static {
+    System.out.println("Static block executes at class loading ");
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
